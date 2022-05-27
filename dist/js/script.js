ded28d01c5275ffd2ebe005183a82681c6f055c7
Emiliya <81705094+ccryptocoder@users.noreply.github.com>
2022-05-27 18:04:17 +0600

    Add promo

---
+++ dist/js/script.js
@@ -0,0 +1,10 @@
+window.addEventListener('DOMContentLoaded', () => {
+    var slider = tns({
+        container: '.promo__slider',
+        items: 1,
+        slideBy: 1,
+        autoplay: true,
+        controls: false,
+        autoplayButtonOutput: false
+    });
+});
