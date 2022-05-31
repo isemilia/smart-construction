3e0dd0af428bdc0721ec16b69b682e637a0ef606
Emiliya <81705094+ccryptocoder@users.noreply.github.com>
2022-05-31 14:11:23 +0600

    Add advantages section

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,26 @@
 window.addEventListener('DOMContentLoaded', () => {
-    var slider = tns({
+    let slider = tns({
         container: '.promo__slider',
         items: 1,
         slideBy: 1,
         autoplay: true,
         controls: false,
         autoplayButtonOutput: false
+    });
+
+    let advSlider = tns({
+        container: '.advantages__slider',
+        items: 2.5,
+        slideBy: 1,
+        autoplay: false,
+        controls: false,
+        autoplayButtonOutput: false,
+        fixedWidth: 570,
+        loop: false,
+        mouseDrag: true,
+        gutter: 40,
+        // center: true,
+        nav: false,
+        swipeAngle: false
     });
 });
