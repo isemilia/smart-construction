e541bd2fc2280dbcf417bf1e4b6552a60c18f106
Emiliya <81705094+ccryptocoder@users.noreply.github.com>
2022-06-07 09:54:12 +0600

    Add ability to change cities

--- dist/js/header.js
+++ dist/js/header.js
@@ -17,3 +17,31 @@ window.addEventListener('DOMContentLoaded', () => {
         })
     })
 });
+
+window.addEventListener('DOMContentLoaded', () => {
+    const city = document.querySelector('[data-city]');
+    const addressNur = document.querySelector('[data-address-nursultan]'),
+    addressAlmaty = document.querySelector('[data-address-almaty]');
+    const mapNur = document.querySelector('[data-map-nursultan]'),
+    mapAlmaty = document.querySelector('[data-map-almaty]');
+
+    city.addEventListener('click', (e) => {
+        e.preventDefault();
+        if(city.innerText == 'Нур-Султан') {
+            city.innerText = 'Алматы';
+            addressNur.style.display = 'none';
+            addressAlmaty.style.display = 'block';
+            //отобразить карту алматы
+            mapNur.style.display = 'none';
+            mapAlmaty.style.display = 'block';
+        } else if (city.innerText == 'Алматы') {
+            city.innerText = 'Нур-Султан';
+            addressNur.style.display = 'block';
+            addressAlmaty.style.display = 'none';
+            //отобразить карту астаны
+            mapNur.style.display = 'block';
+            mapAlmaty.style.display = 'none';
+        }
+    });
+
+});
