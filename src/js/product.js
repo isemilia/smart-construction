0e49056d49f296995d582b3856406fd1a97a18d3
Emiliya <81705094+ccryptocoder@users.noreply.github.com>
2022-07-28 17:48:05 +0600

    Create product page (desktop)

---
+++ src/js/product.js
@@ -0,0 +1,30 @@
+window.addEventListener('DOMContentLoaded', () => {
+    //singleStart
+
+    const buildSwiperSlider = sliderElm => {
+        const sliderIdentifier = sliderElm.dataset.id;
+        return new Swiper(`#${sliderElm.id}`, {
+            navigation: {
+                nextEl: `#contentNext${sliderIdentifier}`,
+                prevEl: `#contentPrev${sliderIdentifier}`
+            },
+            pagination: {
+                el: `#contentPag${sliderIdentifier}`,
+            },
+            breakpoints: {
+                1440: {
+                    slidesPerView: 1.5,
+                    spaceBetween: 20,
+                },
+            },
+        });
+    }
+    
+    // Get all of the swipers on the page
+    const allSliders = document.querySelectorAll('.content-section-swiper');
+    
+    // Loop over all of the fetched sliders and apply Swiper on each one.
+    allSliders.forEach(slider => buildSwiperSlider(slider));
+    
+    //singleEnd
+});
